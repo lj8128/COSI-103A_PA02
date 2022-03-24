4bd23cf56d7ff44485fbb10e130c5ced5805f423
aljoromer <aljoromer@gmail.com>
2022-03-23 20:29:48 -0400

    Add add method to Transcation class and Update __init__

--- pa02/transactions.py
+++ pa02/transactions.py
@@ -1,13 +1,28 @@
 import sqlite3
 
-class Transactions:
-    def __init__(self, filename, itemNum, amount, category, date, description):
+class Transaction:
+    def __init__(self, filename):
+        self.filename = filename
         con = sqlite3.connect(filename)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
                     (itemNum int, amount float, category text, date text, description text)''')
-        cur.execute('''INSERT INTO transcations 
-                    VALUES (?,?,?,?,?)''',
-                    (itemNum, amount, category, date, description))
         con.commit()
         con.close()
+
+    def add(filename,itemNum,amount,category,data,description):
+        ''' add a transaction to the transactions table.
+            this returns the rowid of the inserted element
+        '''
+        con = sqlite3.connect(filename)
+        cur = con.cursor()
+        cur.execute(''' INSERT INTO transcations 
+                        VALUES (?,?,?,?,?)''',
+                        (itemNum, amount, category, data, description))
+        con.commit()
+        cur.execute("SELECT last_insert_rowid()")
+        last_rowid = cur.fetchone()
+        con.commit()
+        con.close()
+        return last_rowid[0]
+            
